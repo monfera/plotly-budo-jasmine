a4b7279e06d351b2dfd13c9695e953658ae80756
Robert Monfera <monfera.robert@gmail.com>
2017-01-06 14:58:02 +0100

    Simplified setup and example access to plotly.js innards e.g. here, Lib

--- my-first-test.js
+++ my-first-test.js
@@ -1,11 +1,32 @@
-// budo --live --open --host localhost my-first-test.js
+// budo --live --open --host localhost my-first-test.js // cheers Ricky Reusser
 
-require('plotly.js').plot(document.body, require('./mock'));
 
-describe("whatever", function() {
-  it('haha', function(done) {
-    expect(1).toEqual(2);
-    console.log('test executed')
-    done();
-  })
+// Jasmine broilerplate - can be put in a separate file
+
+var jasmineReporters = require('jasmine-reporters');
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new jasmineReporters.TapReporter());
+
+
+// Actual test file
+
+var Plotly = require('plotly.js');
+var mock = require('./mock');
+var Lib = require('plotly.js/src/lib');
+
+describe('Parcoords', function() {
+
+  it('renders fine', function(done) {
+
+    var mockCopy = Lib.extendDeep({}, mock);
+    var gd = document.body;
+
+    Plotly.plot(gd, mockCopy).then(function() {
+
+      expect(1).toEqual(1);
+      expect(gd.data.length).toEqual(1);
+
+      done();
+    });
+  });
 });
