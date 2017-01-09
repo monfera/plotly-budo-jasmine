0e4a34f3c81832a99f77326f2715520ca49aefd1
Robert Monfera <monfera.robert@gmail.com>
2017-01-09 10:35:26 +0100

    testing second call to Plotly.plot

--- my-first-test.js
+++ my-first-test.js
@@ -19,14 +19,26 @@ describe('Parcoords', function() {
   it('renders fine', function(done) {
 
     var mockCopy = Lib.extendDeep({}, mock);
+    var mockCopy2 = Lib.extendDeep({}, mock);
     var gd = document.body;
+
+    delete mockCopy.data[0].dimensions[0].constraintrange;
+    delete mockCopy2.data[0].dimensions[0].constraintrange;
+    mockCopy.data[0].dimensions[2].constraintrange = [0, 1];
+    mockCopy2.data[0].dimensions[2].constraintrange = [0, 2];
 
     Plotly.plot(gd, mockCopy).then(function() {
 
       expect(1).toEqual(1);
       expect(gd.data.length).toEqual(1);
 
-      done();
+      Plotly.plot(gd, mockCopy2).then(function() {
+
+        expect(1).toEqual(1);
+        expect(gd.data.length).toEqual(1);
+
+        done();
+      });
     });
   });
 });
