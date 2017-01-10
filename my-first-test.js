1bfe9c7ef83d74e78c99a6f55b664f10f43f67d0
Robert Monfera <monfera.robert@gmail.com>
2017-01-10 15:27:45 +0100

    updating test harness, using DIV

--- my-first-test.js
+++ my-first-test.js
@@ -7,6 +7,22 @@ var jasmineReporters = require('jasmine-reporters');
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(new jasmineReporters.TapReporter());
 
+var gd;
+
+function recreateGraphDiv() {
+
+  gd = document.getElementById('graph');
+  if(gd) document.body.removeChild(gd);
+
+  gd = document.createElement('div');
+  gd.id = 'graph';
+  document.body.appendChild(gd);
+
+  // force the graph to be at position 0,0 no matter what
+  gd.style.position = 'fixed';
+  gd.style.left = 0;
+  gd.style.top = 0;
+};
 
 // Actual test file
 
@@ -17,9 +33,9 @@ var d3 = require('../plotly.js/node_modules/d3');
 
 describe('Parcoords', function() {
 
-  var gd = document.body;
+  beforeEach(recreateGraphDiv);
 
-  describe('final test cases to move to plotly', function() {
+  fdescribe('final test cases to move to plotly', function() {
 
     it('Plotly.deleteTraces with one trace removes the plot', function(done) {
 
@@ -48,13 +64,17 @@ describe('Parcoords', function() {
 
       Plotly.plot(gd, mockCopy).then(function() {
         expect(gd.data.length).toEqual(1);
+        expect(document.querySelectorAll('.panel').length).toEqual(10);
         Plotly.plot(gd, mockCopy2).then(function() {
           expect(gd.data.length).toEqual(2);
+          expect(document.querySelectorAll('.panel').length).toEqual(10 + 7);
           Plotly.deleteTraces(gd, [0]).then(function() {
             expect(document.querySelectorAll('.parcoordsModel').length).toEqual(1);
+            expect(document.querySelectorAll('.panel').length).toEqual(7);
             expect(gd.data.length).toEqual(1);
             Plotly.deleteTraces(gd, 0).then(function() {
               expect(document.querySelectorAll('.parcoordsModel').length).toEqual(0);
+              expect(document.querySelectorAll('.panel').length).toEqual(0);
               expect(gd.data.length).toEqual(0);
               done();
             });
@@ -62,32 +82,60 @@ describe('Parcoords', function() {
         })
       });
     });
-  });
 
-  it('renders fine', function(done) {
+    fdescribe('Having two datasets', function() {
 
-    var mockCopy = Lib.extendDeep({}, mock);
-    var mockCopy2 = Lib.extendDeep({}, mock);
+      it('Two subsequent calls to Plotly.plot should create two parcoords rows', function(done) {
 
-    delete mockCopy.data[0].dimensions[0].constraintrange;
-    delete mockCopy2.data[0].dimensions[0].constraintrange;
-    mockCopy.data[0].dimensions[2].constraintrange = [0, 1];
-    mockCopy2.data[0].dimensions[2].constraintrange = [0, 2];
-    mockCopy2.layout.width = 300;
+        var mockCopy = Lib.extendDeep({}, mock);
+        var mockCopy2 = Lib.extendDeep({}, mock);
+        mockCopy2.data[0].dimensions.splice(3, 4);
 
-    Plotly.plot(gd, mockCopy).then(function() {
+        expect(document.querySelectorAll('.parcoordsModel').length).toEqual(0);
 
-      expect(1).toEqual(1);
-      expect(gd.data.length).toEqual(1);
+        Plotly.plot(gd, mockCopy).then(function() {
 
-     Plotly.plot(gd, mockCopy2).then(function() {
+          expect(1).toEqual(1);
+          expect(document.querySelectorAll('.parcoordsModel').length).toEqual(1);
+          expect(gd.data.length).toEqual(1);
 
-        expect(1).toEqual(1);
-        expect(gd.data.length).toEqual(1);
+          Plotly.plot(gd, mockCopy2).then(function() {
 
-        done();
+            expect(1).toEqual(1);
+            expect(document.querySelectorAll('.parcoordsModel').length).toEqual(2);
+            expect(gd.data.length).toEqual(2);
+
+            done();
+          });
+        });
       });
-    });
+
+      it('Plotly.addTraces should add a new parcoords row', function(done) {
+
+        var mockCopy = Lib.extendDeep({}, mock);
+        var mockCopy2 = Lib.extendDeep({}, mock);
+        mockCopy2.data[0].dimensions.splice(3, 4);
+
+        expect(document.querySelectorAll('.parcoordsModel').length).toEqual(0);
+
+        Plotly.plot(gd, mockCopy).then(function() {
+
+          expect(document.querySelectorAll('.parcoordsModel').length).toEqual(1);
+          expect(gd.data.length).toEqual(1);
+
+          Plotly.addTraces(gd, [mockCopy2.data[0]]).then(function() {
+
+            expect(document.querySelectorAll('.parcoordsModel').length).toEqual(2);
+            expect(gd.data.length).toEqual(2);
+
+            done();
+          });
+        });
+
+      });
+
+    })
+
   });
 
   it('', function(done) {
