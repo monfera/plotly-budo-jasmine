7acd7c103b9fe715db154517b3e1cf2de6e04be6
Robert Monfera <monfera.robert@gmail.com>
2017-01-10 10:28:46 +0100

    deleteTraces tests

--- my-first-test.js
+++ my-first-test.js
@@ -13,14 +13,61 @@ jasmine.getEnv().addReporter(new jasmineReporters.TapReporter());
 var Plotly = require('plotly.js');
 var mock = require('./mock');
 var Lib = require('plotly.js/src/lib');
+var d3 = require('../plotly.js/node_modules/d3');
 
 describe('Parcoords', function() {
+
+  var gd = document.body;
+
+  describe('final test cases to move to plotly', function() {
+
+    it('Plotly.deleteTraces with one trace removes the plot', function(done) {
+
+      var mockCopy = Lib.extendDeep({}, mock);
+
+      mockCopy.data[0].line.showscale = false;
+
+      Plotly.plot(gd, mockCopy).then(function() {
+
+        expect(gd.data.length).toEqual(1);
+
+        Plotly.deleteTraces(gd, 0).then(function() {
+          expect(d3.selectAll('.parcoordsModel').node()).toEqual(null);
+          expect(gd.data.length).toEqual(0);
+          done();
+        });
+      });
+    });
+
+    it('Plotly.deleteTraces with two traces removes the deleted plot', function(done) {
+
+      var mockCopy = Lib.extendDeep({}, mock);
+      var mockCopy2 = Lib.extendDeep({}, mock);
+      mockCopy2.data[0].dimensions.splice(3, 4);
+      mockCopy.data[0].line.showscale = false;
+
+      Plotly.plot(gd, mockCopy).then(function() {
+        expect(gd.data.length).toEqual(1);
+        Plotly.plot(gd, mockCopy2).then(function() {
+          expect(gd.data.length).toEqual(2);
+          Plotly.deleteTraces(gd, [0]).then(function() {
+            expect(document.querySelectorAll('.parcoordsModel').length).toEqual(1);
+            expect(gd.data.length).toEqual(1);
+            Plotly.deleteTraces(gd, 0).then(function() {
+              expect(document.querySelectorAll('.parcoordsModel').length).toEqual(0);
+              expect(gd.data.length).toEqual(0);
+              done();
+            });
+          });
+        })
+      });
+    });
+  });
 
   it('renders fine', function(done) {
 
     var mockCopy = Lib.extendDeep({}, mock);
     var mockCopy2 = Lib.extendDeep({}, mock);
-    var gd = document.body;
 
     delete mockCopy.data[0].dimensions[0].constraintrange;
     delete mockCopy2.data[0].dimensions[0].constraintrange;
@@ -43,34 +90,33 @@ describe('Parcoords', function() {
     });
   });
 
-  fit('renders fine', function(done) {
+  it('', function(done) {
+    var mock = {
+      "data": [
+        {
+          "values": [
+            1,
+            2,
+            3,
+            4,
+            5
+          ],
+          "type": "pie"
+        }
+      ],
+      "layout": {
+        "height": 300,
+        "width": 400
+      }
+    };
 
-    var mockCopy = Lib.extendDeep({}, mock);
-    var mockCopy2 = Lib.extendDeep({}, mock);
-    var gd = document.body;
-
-    delete mockCopy.data[0].dimensions[0].constraintrange;
-    delete mockCopy2.data[0].dimensions[0].constraintrange;
-    mockCopy.data[0].dimensions[2].constraintrange = [0, 1];
-    mockCopy2.data[0].dimensions[2].constraintrange = [0, 2];
-    mockCopy2.layout.width = 1600;
-    mockCopy2.layout.height = 200;
-
-    Plotly.plot(gd, mockCopy).then(function() {
-
-      expect(1).toEqual(1);
-      expect(gd.data.length).toEqual(1);
-
+    Plotly.plot(gd, mock).then(function(){
       Plotly.deleteTraces(gd, 0).then(function() {
-        Plotly.plot(gd, mockCopy2).then(function() {
-
-          expect(1).toEqual(1);
-          expect(gd.data.length).toEqual(1);
-
-          done();
-        });
-      });
+        expect(d3.selectAll('.parcoordsModel').node()).toEqual(null);
+        done();
+      })
     });
-  });
+
+  })
 
 });
